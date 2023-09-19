f8a17bca82955a6b781361397fa0eaccddc870ae
kwonbi <kwonbi@pumpkinev.co.kr>
2023-09-19 13:43:34 +0900

    최초 생성

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+// animations.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    var invitation = document.querySelector(".invitation");
+
+    // 1초 후에 초대장을 열기 위해 클래스 추가
+    setTimeout(function () {
+        invitation.classList.add("open");
+    }, 1000); // 1초 후에 애니메이션 효과 적용
+});
